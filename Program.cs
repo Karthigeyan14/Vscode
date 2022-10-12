e6d0dcf9124b14d36280c39740d5bcb1a1ae8a83
Karthigeyan14 <115165317+Karthigeyan14@users.noreply.github.com>
2022-10-12 15:19:37 +0530

    Hello

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello this");
-Console.WriteLine("Hello this");
+﻿using System;  
+namespace FunctionExample  
+{  
+    class Program  
+    {  
+        // User defined function without return type  
+        public void Show() // No Parameter  
+        {  
+            Console.WriteLine("This is non parameterized function");  
+            // No return statement  
+        }  
+        // Main function, execution entry point of the program  
+        static void Main(string[] args)  
+        {  
+            Program program = new Program(); // Creating Object  
+            program.Show(); // Calling Function             
+        }  
+    }  
+}  
